4b315eac899ac807134f869cd73ef0c909ca6e48
vovav <vovavitka10@gmail.com>
2024-09-20 18:23:07 +0300

    10.2

---
+++ 10.2.py
@@ -0,0 +1,19 @@
+import re
+###########################################################################
+def first_word(text):
+    text = text.lstrip("!@#$%^&*()_-=+,. ")
+    match_word = re.match(r"[\w`']+", text)
+    return match_word.group(0) if match_word else ""
+###########################################################################
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+###########################################################################
+print('OK')
+print(first_word(".Don't fddsfd"))
+print(first_word("  .Don't fddsfd"))
+print(first_word("!.Don't fddsfd"))
+print(first_word("@.Don't fddsfd"))
